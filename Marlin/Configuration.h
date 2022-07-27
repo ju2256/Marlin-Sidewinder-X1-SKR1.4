6c4e186b65fa4e6974758dc4146b5c5899d32cfa
ju2256 <julienquiltu@gmail.com>
2022-07-27 22:41:46 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Cyril Guislain, Sidewinder X1 - SKR 1.4 Turbo)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Cyril Guislain, Sidewinder X1 - SKR 1.4)" // Who made the changes.
 #define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -96,7 +96,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_V1_4_TURBO
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_4
 #endif
 
 /**
